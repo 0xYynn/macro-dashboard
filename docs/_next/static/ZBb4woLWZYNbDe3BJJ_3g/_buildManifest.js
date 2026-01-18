1bcd04ceeca83ce52a402c9f5f0ace52275b0bd6
0xYynn <zulkifli.wrk@gmail.com>
2026-01-18 17:53:39 +0800

    Fix basePath for GitHub Pages

--- docs/_next/static/ZBb4woLWZYNbDe3BJJ_3g/_buildManifest.js
+++ docs/_next/static/ZBb4woLWZYNbDe3BJJ_3g/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/macro-dashboard//_next/:path+",
+        "destination": "/macro-dashboard/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
